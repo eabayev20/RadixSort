1606e50dc63bd826bf65500b61a93c8657220a0c
eabayev20 <eabayev20@gmail.com>
2021-01-13 18:31:38 -0500

    One element of quadratic removed

--- Radix.java
+++ Radix.java
@@ -20,67 +20,28 @@ public static void merge(SortableLinkedList original, SortableLinkedList[] bucke
   }
   public static void radixSortSimple(SortableLinkedList data){
 
-      int maxe = 0;
-      while (data.size() > 0){
-          if (data.get(0) > maxe){
-            maxe = data.remove(0);
+      int maxValue = 0;
+    while(data.size()>0){ //loop through data to get maxValue
+          if (data.get(0) > maxValue){
+            maxValue = data.remove(0);
           }
       }
-      int numsss = length(maxe);
-      for(int i = 0; i < numsss; i ++){
-        SortableLinkedList zero = new SortableLinkedList();
-        SortableLinkedList one = new SortableLinkedList();
-        SortableLinkedList two = new SortableLinkedList();
-        SortableLinkedList three = new SortableLinkedList();
-        SortableLinkedList four = new SortableLinkedList();
-        SortableLinkedList five = new SortableLinkedList();
-        SortableLinkedList six = new SortableLinkedList();
-        SortableLinkedList seven = new SortableLinkedList();
-        SortableLinkedList eight = new SortableLinkedList();
-        SortableLinkedList nine = new SortableLinkedList();
-        while (data.size() > 0){
-      int element = data.remove(0);
-      int num = nth(element,i);
-      if(num ==0) {
-         zero.add(element);
-       }
-      else if(num == 1) {
-        one.add(element);
+      int mostDigits = length(maxValue); //e.g 432, mostdigits would be 3
+      SortableLinkedList[] buckets = new SortableLinkedList[10];
+      for(int i = 0; i < 10; i ++){
+        buckets[i] = new SortableLinkedList();
       }
-      else if(num == 2) {
-        two.add(element);
+
+      for(int i = 0; i < mostDigits; i ++){
+        while(data.size()>0){
+          int ele =data.remove(0);
+          int digit = nth(ele,i);
+          buckets[digit].add(ele);
+
+        }
+        merge(data,buckets);
       }
-      else if(num == 3) {
-        three.add(element);
-      }
-      else if(num == 4) {
-        four.add(element);
-      }
-      else if(num == 5) {
-        five.add(element);
-      }
-      else if(num == 6) {
-        six.add(element);
-      }
-      else if(num == 7) {
-        seven.add(element);
-      }
-      else if(num == 8) {
-        eight.add(element);
-      }
-      else {
-        nine.add(element);
-      }
-    }
-    SortableLinkedList temp = new SortableLinkedList();
-    SortableLinkedList[] buckets = new SortableLinkedList[]{zero,one,two,three,four,five,six,seven,eight,nine};
-    Radix.merge(temp,buckets);
-    for(int j = data.size()-1; j >= 0; j --){
-      data.remove(j);
-    }
-    data.extend(temp);
 
   }
-}
 public static void radixSort(SortableLinkedList data) {}
 }
