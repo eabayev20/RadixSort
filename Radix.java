6576f5a19d15775ef51acf2481923355f249bbe4
eabayev20 <eabayev20@gmail.com>
2021-01-13 11:29:35 -0500

    radix done with log

--- Radix.java
+++ Radix.java
@@ -4,6 +4,11 @@ public class Radix{
 
     return (int) (Math.abs(n) / Math.pow(10, col)) % 10;
   }
-
+  public static int length(int n){
+    if(n==0){
+      return 0;
+    }
+    return (int) (Math.log10(Math.abs(n)) + 1);
+  }
 
 }
