21c2ba9744b806779aab1cff05bbdc1ec0dd15a7
eabayev20 <eabayev20@gmail.com>
2021-01-13 11:31:46 -0500

    merge done

--- Radix.java
+++ Radix.java
@@ -5,10 +5,36 @@ public class Radix{
     return (int) (Math.abs(n) / Math.pow(10, col)) % 10;
   }
   public static int length(int n){
-    if(n==0){
-      return 0;
+    int r = 0;
+    if(n == 0) {
+      return r;
     }
+    else {
     return (int) (Math.log10(Math.abs(n)) + 1);
   }
+}
+public static void merge(SortableLinkedList original, SortableLinkedList[] buckets){
+    for(int i = 0; i < buckets.length; i ++){
+      original.extend(buckets[i]);
+    }
+  }
+
+  public static void radixSortSimple(SortableLinkedList data){
+
+    int maxValue = 0;
+    for(int i = 0; i < data.size(); i ++){ //loop through data to get maxValue
+        if (data.get(i) > maxValue){
+          maxValue = data.get(i);
+        }
+    }
+    int mostDigits = length(maxValue); //e.g 432, mostdigits would be 3
+    for(int i = 0; i < mostDigits; i ++){
+      SortableLinkedList zero = new SortableLinkedList();
+      SortableLinkedList one = new SortableLinkedList();
+      SortableLinkedList two = new SortableLinkedList();
+      SortableLinkedList three = new SortableLinkedList();
+      SortableLinkedList four = new SortableLinkedList();
+      SortableLinkedList five = new SortableLinkedList();
+      SortableLinkedList six = new 
 
 }
